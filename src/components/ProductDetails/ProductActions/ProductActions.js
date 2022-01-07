a76ef355b044a80df2f86610ca23a9522b18d226
Radoslaw Laszek <radek.laszek1999@gmail.com>
2022-01-07 19:31:39 +0100

    ProductQuantity.js finished

--- src/components/ProductDetails/ProductActions/ProductActions.js
+++ src/components/ProductDetails/ProductActions/ProductActions.js
@@ -1,11 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import ProductQuantity from "./ProductQuantity";
+import { useState } from "react";
 
 const ProductActions = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  const plusHandler = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  const minusHandler = () => {
+    setQuantity((prevState) => (prevState === 0 ? prevState : prevState - 1));
+  };
+
   return (
     <div className=" flex flex-col md:flex-row gap-3">
-      <ProductQuantity />
+      <ProductQuantity
+        quantity={quantity}
+        plus={plusHandler}
+        minus={minusHandler}
+      />
       <div className=" cursor-pointer h-12 flex gap-3 justify-center items-center grow bg-orange text-white font-bold rounded-md shadow-add-to-cart shadow-orange hover:opacity-50">
         <FontAwesomeIcon icon={faShoppingCart} />
         Add to cart
--- src/components/ProductDetails/ProductActions/ProductQuantity.js
+++ src/components/ProductDetails/ProductActions/ProductQuantity.js
@@ -1,12 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
-const ProductQuantity = () => {
+const ProductQuantity = (props) => {
+  const { quantity, plus, minus } = props;
+
   return (
     <div className=" flex items-center justify-between md:w-32 h-12 px-5 md:px-2 bg-light-grayish-blue rounded-md font-bold">
-      <FontAwesomeIcon icon={faMinus} className="text-orange cursor-pointer" />
-      <div>{productQuantity}</div>
-      <FontAwesomeIcon icon={faPlus} className="text-orange cursor-pointer" />
+      <FontAwesomeIcon
+        icon={faMinus}
+        className="text-orange cursor-pointer"
+        onClick={minus}
+      />
+      <div>{quantity}</div>
+      <FontAwesomeIcon
+        icon={faPlus}
+        className="text-orange cursor-pointer"
+        onClick={plus}
+      />
     </div>
   );
 };
